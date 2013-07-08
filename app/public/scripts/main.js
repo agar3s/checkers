c917ffff6f306ad7dbff4033d166d8721e8d3c74
Agares <ekeisco@gmail.com>
2013-07-08 16:59:13 -0500

    checking items

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,10 +1,28 @@
 $('document').ready( function(){
   
   $('a.expand').on('click', function(e){
-    var algo = $(this).data('items');
-    console.log(algo);
-    $("#"+algo).slideToggle(500);
+    var items = $(this).data('items');
+    var hidden = $('#'+items).is(':hidden');
+    $(this).html(hidden?'-':'+');
+    $('#'+items).slideToggle(500);
     return false;
+  });
+
+  $('.check-progress').on('change', function(e){
+    var checkbox = e.target;
+    console.log($(checkbox).parent());
+    if(!$(checkbox).is(':checked')){
+      $('#'+checkbox.id+'~span').html('0%');
+      return false;
+    }
+    $('#'+checkbox.id+'~span').html('100%');
+    //$('#'+checkbox.id+'~a.expand').click();
+
+    var childs = $('[id^="'+checkbox.id+'-"]');
+    for(var i=0; i<childs.length; i++){
+      $(childs[i]).prop('checked', true);
+      $('#'+childs[i].id+'~span').html('100%');
+    }
   });
 
 });
