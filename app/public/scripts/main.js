bf321aa1e630c68fc974f8f5fd13d0a7976d130e
Agares <ekeisco@gmail.com>
2013-07-09 00:44:35 -0500

    better display with a cool example

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -8,21 +8,61 @@ $('document').ready( function(){
     return false;
   });
 
+  var updateParent = function(parentId, checked){
+    var parent = $('#dt-'+parentId);
+
+    var data = parent.data();
+    var childs;
+    if(data==null){
+      parent = $('.list')[0];
+      childs = $(parent).children('dl').children('dt');
+      var relativeProgress=0;
+      for (var i = childs.length - 1; i >= 0; i--) {
+        relativeProgress += parseInt($(childs[i]).attr('data-progress'));
+      };
+      $('#root').attr('value', relativeProgress/childs.length);
+      return;
+    }
+    
+    if(data.childs==0){
+      parent.attr('data-progress', checked?100:0);
+    }else{
+      var relativeProgress=0;
+      childs = parent.children('dl').children('dt');
+      for (var i = childs.length - 1; i >= 0; i--) {
+        relativeProgress += parseInt($(childs[i]).attr('data-progress'));
+      };
+      parent.attr('data-progress', relativeProgress/data.childs);
+    }
+    if(checked){
+      $('#'+parentId+'~span').html('100%');
+    }else{
+      var strProgress = ''+parent.attr('data-progress');
+      $('#'+parentId+'~span').html(strProgress.substring(0, 5)+'%');
+    }
+
+    
+    updateParent(parent.data('parent-id'));
+
+  };
+
   $('.check-progress').on('change', function(e){
     var checkbox = e.target;
-    console.log($(checkbox).parent());
-    if(!$(checkbox).is(':checked')){
-      $('#'+checkbox.id+'~span').html('0%');
+    var parentId = $(checkbox).parent().data('parent-id');
+    var checked = $(checkbox).is(':checked');
+    
+    
+    if(!checked){
+      updateParent(checkbox.id, checked);
       return false;
     }
-    $('#'+checkbox.id+'~span').html('100%');
-    //$('#'+checkbox.id+'~a.expand').click();
-
     var childs = $('[id^="'+checkbox.id+'-"]');
     for(var i=0; i<childs.length; i++){
       $(childs[i]).prop('checked', true);
+      $('#dt-'+childs[i].id).attr('data-progress', 100);
       $('#'+childs[i].id+'~span').html('100%');
     }
+    updateParent(checkbox.id, checked);
   });
 
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,9 @@
  */
 
 exports.index = function(req, res){
-  var parsedJSON = require('../public/lists/sample.json');
-  res.render('index', { title: 'The Checkers Project', items: parsedJSON });
+  var parsedJSON = require('../public/lists/webdevChecklist.json');
+  res.render('index', { title: 'Web checklist',
+                        description:'This is a simple list of the things that you need to achieve'+
+                        'in order to become a great web developer',
+                        items: parsedJSON });
 };
